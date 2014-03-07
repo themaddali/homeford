ca3351b0d8870f02fa9ee4d7a26d4bb9ca37a07c
Venkat Maddali <maddali@outlook.com>
2014-03-07 15:03:51 -0800

    Adding new homeview images

--- webapps/univ/js/app/HomeView.js
+++ webapps/univ/js/app/HomeView.js
@@ -5,7 +5,7 @@ define(['spin', 'cookie', 'plugins', 'flatvid', 'typeahead', 'bloodhound', '../a
 	var HomeView = ( function() {
 
 			var PARAM = {
-				"Bg" : ["img\/1.jpg", "img\/2.jpg", "img\/3.jpg", "img\/4.jpg", "img\/5.jpg"]
+				"Bg" : ["img\/1.jpg", "img\/2.jpg", "img\/3.jpg", "img\/4.jpg", "img\/5.jpg", "img\/6.jpg", "img\/7.jpg"]
 			};
 
 			var EDIT = '<i id="entity-edit" style="padding-left:10px;font-size:10px; display:none; vertical-align:super;" class="icon-gear  icon-1x ">Change</i>';
