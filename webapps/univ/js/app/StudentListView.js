a22128d7c5238d0ae18fc96e2e1a9f63758d7a2e
Venkat Maddali <maddali@outlook.com>
2014-03-11 17:51:05 -0500

    Style Change

--- webapps/univ/js/app/StudentListView.js
+++ webapps/univ/js/app/StudentListView.js
@@ -55,14 +55,6 @@ define(['modernizr', 'spin', 'plugins', 'cookie', '../app/service/DataService', 
 								if (i === COUNT - 1) {
 									//jQuery('#carousel').append('<div class="empty"></div>');
 									//createPanels();
-									if (COUNT > 12)
-									{
-										jQuery('#searchbar').addClass('active');
-									}
-									else
-									{
-										jQuery('#searchbar').removeClass('active');
-									}
 									jQuery("#preloader").hide();
 									ActivatePanelEvents();
 								}
