eb919ca3d537c66a73b8d16405bbc79c09e33d91
Venkat Maddali <maddali@outlook.com>
2014-03-25 14:31:19 -0700

    home page trials

--- webapps/univ/js/app/service/DataService.js
+++ webapps/univ/js/app/service/DataService.js
@@ -55,53 +55,56 @@ define(['jquery'], function() {"use strict";
 							}
 							handlers.success(imagelist);
 						},
-						error : function() {
-							var sampleresponse = jsonFlickrFeed({
-								"title" : "Recent Uploads tagged book",
-								"link" : "http://www.flickr.com/photos/tags/book/",
-								"description" : "",
-								"modified" : "2014-03-23T15:10:42Z",
-								"generator" : "http://www.flickr.com/",
-								"items" : [{
-									"title" : "a guide to life",
-									"link" : "http://www.flickr.com/photos/8999293@N03/13354974265/",
-									"media" : {
-										"m" : "http://farm8.staticflickr.com/7061/13354974265_df9300e760_m.jpg"
-									},
-									"date_taken" : "2014-03-23T07:50:09-08:00",
-									"description" : " <p><a href=\"http://www.flickr.com/people/8999293@N03/\">barbara carroll<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/8999293@N03/13354974265/\" title=\"a guide to life\"><img src=\"http://farm8.staticflickr.com/7061/13354974265_df9300e760_m.jpg\" width=\"240\" height=\"160\" alt=\"a guide to life\" /><\/a><\/p> <p>I find it hard to resist books like this. This was published in 1884. The first paragraph is: The Home Instructor is intended to be a friendly adviser, rather than a stern dictator. The results of many years of experience and observation are gathered in these pages, with the hope they may prove both pleasant and helpful. Great stress is laid on the value of home training, from the deepening conviction that as the home is, so the life will be.<\/p>",
-									"published" : "2014-03-23T15:10:42Z",
-									"author" : "nobody@flickr.com (barbara carroll)",
-									"author_id" : "8999293@N03",
-									"tags" : "cookies book tea vintagebook oatmealcranberrycookies"
-								}, {
-									"title" : "Puglia",
-									"link" : "http://www.flickr.com/photos/leonardino23/13352742554/",
-									"media" : {
-										"m" : "http://farm8.staticflickr.com/7355/13352742554_ea024ae252_m.jpg"
-									},
-									"date_taken" : "2014-03-15T15:10:55-08:00",
-									"description" : " <p><a href=\"http://www.flickr.com/people/leonardino23/\">Leonardino23<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/leonardino23/13352742554/\" title=\"Puglia\"><img src=\"http://farm8.staticflickr.com/7355/13352742554_ea024ae252_m.jpg\" width=\"240\" height=\"135\" alt=\"Puglia\" /><\/a><\/p> ",
-									"published" : "2014-03-23T13:12:22Z",
-									"author" : "nobody@flickr.com (Leonardino23)",
-									"author_id" : "95417157@N08",
-									"tags" : "book nikon italia libro puglia emiliaromagna holday d7100 nikond7100"
-								}, {
-									"title" : "Lecture au soleil (Mars 2014)",
-									"link" : "http://www.flickr.com/photos/75891446@N07/13352022955/",
-									"media" : {
-										"m" : "http://farm4.staticflickr.com/3800/13352022955_8a9d9e09df_m.jpg"
-									},
-									"date_taken" : "2014-03-13T14:10:06-08:00",
-									"description" : " <p><a href=\"http://www.flickr.com/people/75891446@N07/\">Ostrevents<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/75891446@N07/13352022955/\" title=\"Lecture au soleil (Mars 2014)\"><img src=\"http://farm4.staticflickr.com/3800/13352022955_8a9d9e09df_m.jpg\" width=\"240\" height=\"135\" alt=\"Lecture au soleil (Mars 2014)\" /><\/a><\/p> <p>Pose lecture au soleil de Notre Dame à Paris.<\/p>",
-									"published" : "2014-03-23T13:04:59Z",
-									"author" : "nobody@flickr.com (Ostrevents)",
-									"author_id" : "75891446@N07",
-									"tags" : "sun man paris france tree seine square book soleil europa europe reader young notredame read lecture arbre livre youngman homme lecteur jeune jeunehomme chn ostrevents nddame"
-								}]
-							})
-							handlers.success(sampleresponse);
+						error : function(){
+							handlers.error();
 						}
+						// error : function() {
+							// var sampleresponse = jsonFlickrFeed({
+								// "title" : "Recent Uploads tagged book",
+								// "link" : "http://www.flickr.com/photos/tags/book/",
+								// "description" : "",
+								// "modified" : "2014-03-23T15:10:42Z",
+								// "generator" : "http://www.flickr.com/",
+								// "items" : [{
+									// "title" : "a guide to life",
+									// "link" : "http://www.flickr.com/photos/8999293@N03/13354974265/",
+									// "media" : {
+										// "m" : "http://farm8.staticflickr.com/7061/13354974265_df9300e760_m.jpg"
+									// },
+									// "date_taken" : "2014-03-23T07:50:09-08:00",
+									// "description" : " <p><a href=\"http://www.flickr.com/people/8999293@N03/\">barbara carroll<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/8999293@N03/13354974265/\" title=\"a guide to life\"><img src=\"http://farm8.staticflickr.com/7061/13354974265_df9300e760_m.jpg\" width=\"240\" height=\"160\" alt=\"a guide to life\" /><\/a><\/p> <p>I find it hard to resist books like this. This was published in 1884. The first paragraph is: The Home Instructor is intended to be a friendly adviser, rather than a stern dictator. The results of many years of experience and observation are gathered in these pages, with the hope they may prove both pleasant and helpful. Great stress is laid on the value of home training, from the deepening conviction that as the home is, so the life will be.<\/p>",
+									// "published" : "2014-03-23T15:10:42Z",
+									// "author" : "nobody@flickr.com (barbara carroll)",
+									// "author_id" : "8999293@N03",
+									// "tags" : "cookies book tea vintagebook oatmealcranberrycookies"
+								// }, {
+									// "title" : "Puglia",
+									// "link" : "http://www.flickr.com/photos/leonardino23/13352742554/",
+									// "media" : {
+										// "m" : "http://farm8.staticflickr.com/7355/13352742554_ea024ae252_m.jpg"
+									// },
+									// "date_taken" : "2014-03-15T15:10:55-08:00",
+									// "description" : " <p><a href=\"http://www.flickr.com/people/leonardino23/\">Leonardino23<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/leonardino23/13352742554/\" title=\"Puglia\"><img src=\"http://farm8.staticflickr.com/7355/13352742554_ea024ae252_m.jpg\" width=\"240\" height=\"135\" alt=\"Puglia\" /><\/a><\/p> ",
+									// "published" : "2014-03-23T13:12:22Z",
+									// "author" : "nobody@flickr.com (Leonardino23)",
+									// "author_id" : "95417157@N08",
+									// "tags" : "book nikon italia libro puglia emiliaromagna holday d7100 nikond7100"
+								// }, {
+									// "title" : "Lecture au soleil (Mars 2014)",
+									// "link" : "http://www.flickr.com/photos/75891446@N07/13352022955/",
+									// "media" : {
+										// "m" : "http://farm4.staticflickr.com/3800/13352022955_8a9d9e09df_m.jpg"
+									// },
+									// "date_taken" : "2014-03-13T14:10:06-08:00",
+									// "description" : " <p><a href=\"http://www.flickr.com/people/75891446@N07/\">Ostrevents<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/75891446@N07/13352022955/\" title=\"Lecture au soleil (Mars 2014)\"><img src=\"http://farm4.staticflickr.com/3800/13352022955_8a9d9e09df_m.jpg\" width=\"240\" height=\"135\" alt=\"Lecture au soleil (Mars 2014)\" /><\/a><\/p> <p>Pose lecture au soleil de Notre Dame à Paris.<\/p>",
+									// "published" : "2014-03-23T13:04:59Z",
+									// "author" : "nobody@flickr.com (Ostrevents)",
+									// "author_id" : "75891446@N07",
+									// "tags" : "sun man paris france tree seine square book soleil europa europe reader young notredame read lecture arbre livre youngman homme lecteur jeune jeunehomme chn ostrevents nddame"
+								// }]
+							// })
+							// handlers.success(sampleresponse);
+						// }
 					});
 				}
 
