d6bc7e2c1ad900d2576cff08bbcd2e1b29023976
Venkat Maddali <maddali@outlook.com>
2014-07-27 15:44:27 -0700

    Today cleanup for preview

--- webapps/univ/js/app/view/billing/InvoicePreviewView.js
+++ webapps/univ/js/app/view/billing/InvoicePreviewView.js
@@ -76,7 +76,7 @@ define(['cookie', '../../service/DataService', 'validate', '../../Router', '../.
 						var day = currentDate.getDate();
 						var month = currentDate.getMonth() + 1;
 						if (month < 10) {
-							month = 0 + month;
+							month = '0' + month;
 						}
 						if (day < 10) {
 							day = '0' + day;
